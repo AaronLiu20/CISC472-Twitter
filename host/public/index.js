3b054ad18e2415330ddd2d928b0f787275df1bb0
Aaronliu20 <Olympus@udel.edu>
2022-10-10 00:24:18 -0400

    Added login and user functionality

--- host/public/index.js
+++ host/public/index.js
@@ -10,34 +10,32 @@
 
   //Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-  const TweetDBR = firebase.database().ref("/Tweets");
-  const ArchieveDBR  = firebase.database().ref("/Archieved");
-  const UserDBR = firebase.database().ref("/Users");
+const TweetDBR = firebase.database().ref("/Tweets");
+const ArchieveDBR  = firebase.database().ref("/Archieved");
+const UserDBR = firebase.database().ref("/Users");
+const UsernameDBR = firebase.database().ref("/Usernames");
+let provider = new firebase.auth.GoogleAuthProvider();
 
   //this is a helper function that does wraps the object into a new object and saves it into the database 
-  let UpdateData = (originalTweet, id) => {
-    firebase.database().ref(TweetDBR).child(id).update(originalTweet);
-  }
+let UpdateData = (placement, id, content) => {
+  firebase.database().ref(placement).child(id).update(content);
+}
 
   //this is the helper function that appends the form to edit/create a new tweet and attaches the sumbit function to the submit button
-  let insertForm = (id, creating) => {
+  let insertForm = (id, creating, userUID) => {
     document.getElementById("insert-form").innerHTML = "";
     document.getElementById("insert-form").insertAdjacentHTML("beforeend", (`
-    <form id = "tweetForm">
-      <label>Author:</label><br>
-      <input type = "text" id = "UserHandle" placeholder = "Author.handle"></input><br>
-      <label>Picture</label><br>
-      <input type = " text" id = "Author.picture" placeholder = "Author.picture" value = "https://www.svg.com/img/gallery/the-scariest-games-markiplier-has-ever-played/intro-1659295045.jpg"></input><br>
+    <form id = "Creating-Tweet">
       <label>Content:</label><br>
-      <textarea type = "text" id = "Content" placeholder = "Content" rows = "4" cols = "50%"></textarea><br>
+      <textarea type = "text" id = "Content" placeholder = "Content" rows = "4" cols = "50%" required ></textarea><br>
       <label> Tweet: </label>
-      <button id = "FormSubmit">Submit</button>
+      <input type = "submit"></input>
     </form>`));
-    document.getElementById("FormSubmit").onclick = function(){SubmitTweets(id, creating);};
+    document.getElementById("Creating-Tweet").onsubmit = function(){SubmitTweets(id, creating, userUID);};
   }
 
 //this this appends the tweet on the left side and creates the html with the proper information
-let renderTweets = (tweetObject, TweetID, placement)=>{
+let renderTweets = (tweetObject, TweetID, userObj, placement, sameUser)=>{
   let hours =  (new Date() - new Date(tweetObject.timestamp))/3600000;
   document.getElementById(placement).insertAdjacentHTML("beforeend",(`
       <div id = "insertTweets"> </div>
@@ -45,17 +43,17 @@ let renderTweets = (tweetObject, TweetID, placement)=>{
       <div class = "tweetcard" id = "${TweetID}">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src=${tweetObject.Author.picture} class="img-fluid rounded-start" alt="...">
+            <img src=${userObj.picture} class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <p class="card-text"><small class="text-muted" id = "Handle-${TweetID}" >${tweetObject.Author.handle}</small></p>
+              <p class="card-text"><small class="text-muted" id = "Handle-${TweetID}" >${userObj.username}</small></p>
               <p class="card-text" id  = "Content-${TweetID}">${tweetObject.content}</p>
               <p class="card-text"><small class="text-muted" id = "timestamp-${TweetID}" >${new Date(tweetObject.timestamp).toLocaleString()}</small></p>
               <div class = "btn-group">
-                <button class = "Likes" id = "Likes-${TweetID}"><small>likes: ${tweetObject.like}</small></button>
-                <button class = "Delete" id = "delete-${TweetID}"><small>Delete</small></button>
-                <button class = "Edit" id = "Edits-${TweetID}"><small>edit</small></button>
+                <button class = "Button" id = "Likes-${TweetID}"><small>likes: ${tweetObject.like}</small></button>
+                ${sameUser ? `<button class = "Button" id = "delete-${TweetID}"><small>Delete</small></button>
+                <button class = "Button" id = "Edits-${TweetID}"><small>edit</small></button>`: `<button class = "Button" id = "Retweet-${TweetID}"><small>Retweet</small></button>`}
               </div>
             </div>
           </div>
@@ -65,79 +63,141 @@ let renderTweets = (tweetObject, TweetID, placement)=>{
 };
 
 //this updates the like field and updates it on the html
-let LikeListener = (originalTweet, id)=>{
-  document.getElementById(`Likes-${id}`).addEventListener("click", function(event){
-    originalTweet.like += 1; 
-    UpdateData(originalTweet, id);
-    document.getElementById(`Likes-${id}`).innerHTML = `<small>likes: ${originalTweet.like}</small>`;
+let LikeListener = (originalTweet, tweetId, userUID)=>{
+  document.getElementById(`Likes-${tweetId}`).addEventListener("click", function(event){
+    firebase.database().ref(UserDBR).child(userUID).child("Like").once("value").then( function(ss){
+      //this the dislike functionality
+      if(ss.child(tweetId).exists()){
+        firebase.database().ref(UserDBR).child(userUID).child("Like").child(tweetId).remove();
+        originalTweet.like -= 1;
+      }
+      //this is the like functionaility
+      else{
+        let temp = {CreatorUID: originalTweet.uid};
+        firebase.database().ref(UserDBR).child(userUID).child("Like").child(tweetId).update(temp);
+        originalTweet.like += 1; 
+      }
+      UpdateData(TweetDBR, tweetId, originalTweet);
+      document.getElementById(`Likes-${tweetId}`).innerHTML = `<small>likes: ${originalTweet.like}</small>`;
+    });
   });
 }
 
 //toggles the deleted field to true or false and removes it from the page if it is deleted
-let Deletelistener = (originalTweet, id) =>{
+let Deletelistener = (originalTweet, id, userUID) =>{
   document.getElementById(`delete-${id}`).addEventListener("click", function(event){
-    let idividualTweet = firebase.database().ref(`Tweets/${id}`);
     let reference = document.getElementById(id);
     reference.removeEventListener("click", LikeListener);
     reference.removeEventListener("click", Deletelistener);
     reference.removeEventListener("click", EditListener);
+    firebase.database().ref(UserDBR).child(userUID).child("TweetID").child(id).remove();
     firebase.database().ref(ArchieveDBR).push(originalTweet);
     firebase.database().ref(TweetDBR).child(id).remove();
     reference.remove();
   });
 }
 
-function SubmitTweets(id, CreatingNew){
-  let tempHandle = document.getElementById("UserHandle").value;
-  let tempAuthorPicture = document.getElementById("Author.picture").value;
+//this gets the form data and formats into a tweet that will be saved within the database
+function SubmitTweets(id, CreatingNew, userUID){
   let tempContent = document.getElementById("Content").value;
   let newKey = id;
   if(CreatingNew){
     newKey = firebase.database().ref(TweetDBR).push('posts').key;
+    let temp = {content: tempContent}
+    firebase.database().ref(UserDBR).child(userUID).child("TweetID").child(newKey).update(temp);
   }
   let TweetTimeStamp = new Date().getTime();
-
-  //this creates a new json object with all of the information for the new tweets
   let newTweet = {
-    Author: {
-      handle: tempHandle,
-      picture: tempAuthorPicture
-    },
+    uid: userUID,
     content: tempContent,
     like: 0,
     timestamp: TweetTimeStamp,
   };
-  UpdateData(newTweet, newKey);
+  UpdateData(TweetDBR, newKey, newTweet);
 }
 
-let EditListener = (originalTweet, id) =>{
+let twitterLogin = () => {
+  document.getElementById("insert-form").innerHTML = "";
+  document.getElementById("insert-form").insertAdjacentHTML("beforeend", (`
+    <form id = "loginForm">
+      <label>Author:</label><br>
+      <input type = "text" id = "newHandle" placeholder = "Author.handle" required></input><br>
+      <label>Picture</label><br>
+      <input type = " text" id = "newPicture" placeholder = "Author.picture" value = "https://www.svg.com/img/gallery/the-scariest-games-markiplier-has-ever-played/intro-1659295045.jpg" required ></input><br>
+      <label> Account: </label>
+      <input type = "submit"></input>
+    </form>`));
+  document.getElementById("loginForm").onsubmit = (e) => {
+    e.preventDefault();
+    let newHandle = document.getElementById("newHandle").value;
+    let newPicture = document.getElementById("newPicture").value;
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      let newUser = {
+        username: newHandle,
+        picture: newPicture,
+        email: result.user.email
+      };
+
+      let newUsername = {
+        uid: result.user.uid
+      }
+      firebase.database().ref( UserDBR).once("value").then( (ss) => {
+        if(!ss.child(result.user.uid).exists()){
+          UpdateData(UserDBR, result.user.uid, newUser);
+          updateData(UsernameDBR, newHandle, newUsername);
+        }
+      });
+
+    }).catch((error) => {alert(error);});
+  };
+}
+
+//this injects the id into the submit function and updates the information inside of the ID
+let EditListener = (originalTweet, id, userUID ) =>{
   document.getElementById(`Edits-${id}`).addEventListener("click", function(event){
-    insertForm(id, false);
-    document.getElementById("UserHandle").value = originalTweet.Author.handle;
-    document.getElementById("Author.picture").value = originalTweet.Author.picture;
+    event.preventDefault();
+    insertForm(id, false, userUID);
     document.getElementById("Content").value = originalTweet.content;
   });
 }
 
-//reads the databse for tweets and fires the renderTweet with the data it gets*/
-TweetDBR.on( "child_added" , (ss) =>{
-  let tempTweet = ss.val();
-  renderTweets(tempTweet, ss.key, "insertTweets");
-  LikeListener(tempTweet, ss.key);
-  Deletelistener(tempTweet, ss.key);
-  EditListener(tempTweet, ss.key);
-});
-
+//this is the start of the whole program since almost everything is now attached to a uid
 firebase.auth().onAuthStateChanged(user=>{
-  if(!user){
-  document.getElementById("Writing-Tweets").addEventListener("click", function(event){
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  });
+  let loginReference = document.getElementById("login");
+  if(user){
+    //this reads and shows all of the teeks that are current stored
+    TweetDBR.on("child_added" , (ss) =>{
+      let tempTweet = ss.val();
+      firebase.database().ref(UserDBR).child(ss.val().uid).get().then( (result) =>{
+        let sameUser = (tempTweet.uid === user.uid);
+        renderTweets(tempTweet, ss.key, result.val(), "insertTweets", sameUser);
+        if(sameUser){
+          LikeListener(tempTweet, ss.key, user.uid);
+          Deletelistener(tempTweet, ss.key, user.uid);
+          EditListener(tempTweet, ss.key, user.uid);
+        }
+      });
+    });
+
+    //this addes the form to after logged in
+    document.getElementById("Writing-Tweets").onclick = function(event){
+      event.preventDefault();
+      insertForm(0, true, user.uid);
+    }
+    loginReference.innerHTML = "Sign Out";
+    loginReference.onclick = function(){
+      firebase.auth().signOut();
+    };
   }
   else{
-    document.getElementById("Writing-Tweets").addEventListener("click", function(event){
-      insertForm(0, true);
-    });
+    //this prevents the create tweet button from adding the form and alert the user to login
+    document.getElementById("Writing-Tweets").onclick = function(event){
+      alert("You need to log in to create a tweet");
+    }
+    document.getElementById("insertTweets").innerHTML = '';
+    loginReference.innerHTML = "Sign in";
+    loginReference.onclick = function(event){
+      twitterLogin();
+    };
   }
 });
